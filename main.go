3f89d6134be8f6339b03618eaacbf00a22beeb16
Renato Cipolla <renato.cipolla@bjss.com>
2024-09-30 12:11:41 +0100

    add TestPlayerMovement

--- main.go
+++ main.go
@@ -4,6 +4,21 @@ import (
 
 )
 
+type Room struct {
+	Name        string
+	Description string
+	Exits       map[string]*Room
+	Items       []string
+}
+
+type Player struct {
+	CurrentRoom *Room
+	Inventory   []string
+}
+
+func (p *Player) Move(direction string) {
+	p.CurrentRoom = p.CurrentRoom.Exits[direction]
+}
+
 func main() {
-    
 }
---
+++ main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPlayerMovement(t *testing.T) {
+	//Arrange
+	room1 := Room{Name: "Room 1", Description: "This is room 1.", Exits: make(map[string]*Room)}
+    room2 := Room{Name: "Room 2", Description: "This is room 2.", Exits: make(map[string]*Room)}
+    room1.Exits["north"] = &room2
+    room2.Exits["south"] = &room1
+
+    player := Player{CurrentRoom: &room1}
+
+    // Act
+    player.Move("north")
+
+    // Assert
+    if player.CurrentRoom.Name != "Room 2" {
+        t.Errorf("Expected Room 2, got %s", player.CurrentRoom.Name)
+    }
+}
