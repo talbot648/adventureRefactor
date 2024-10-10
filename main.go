61a27e32be421580b2636fa9060e2c4da4cb04f4
Charles Talbot <charles.talbot@bjss.com>
2024-10-10 10:41:05 +0100

    spearated valid interactions from main into its own function

--- main.go
+++ main.go
@@ -315,43 +315,7 @@ func main() {
 
 	introductionShown := false
 
-	validInteractions = []*Interaction{
-		{
-			ItemName:   "tea",
-			EntityName: "rosie",
-			Event:      &Event{Description: "get-your-lanyard", Outcome: "Cheers! I needed that... by the way, where is your lanyard? I must have forgotten to give it to you.\nYou'll need that to move between rooms, here it is.\n\n(lanyard can now be found in the room).\n", Triggered: false},
-		},
-		{
-			ItemName:   "first-plate",
-			EntityName: "dishwasher",
-			Event:      &Event{Description: "first-plate-loaded", Outcome: "You loaded the first plate into the dishwasher.", Triggered: false},
-		},
-		{
-			ItemName:   "second-plate",
-			EntityName: "dishwasher",
-			Event:      &Event{Description: "second-plate-loaded", Outcome: "You loaded the second plate into the dishwasher.", Triggered: false},
-		},
-		{
-			ItemName:   "third-plate",
-			EntityName: "dishwasher",
-			Event:      &Event{Description: "third-plate-loaded", Outcome: "You loaded the third plate into the dishwasher.", Triggered: false},
-		},
-		{
-			ItemName:   "fourth-plate",
-			EntityName: "dishwasher",
-			Event:      &Event{Description: "fourth-plate-loaded", Outcome: "You loaded the fourth plate into the dishwasher.", Triggered: false},
-		},
-		{
-			ItemName:   "fifth-plate",
-			EntityName: "dishwasher",
-			Event:      &Event{Description: "fifth-plate-loaded", Outcome: "You loaded the fifth plate into the dishwasher.", Triggered: false},
-		},
-		{
-			ItemName:   "sixth-plate",
-			EntityName: "dishwasher",
-			Event:      &Event{Description: "sixth-plate-loaded", Outcome: "You loaded the sixth plate into the dishwasher.", Triggered: false},
-		},
-	}
+	validInteraction()
 
 	dishwasherChallengeWon := &Event{Description: "dishwasher-loaded", Outcome: "You load the dirty plates into the dishwasher and switch it on, a feeling of being used washing over you.\nThis challenge felt less like teamwork and more like being roped into someone else's mess.\nWith a sigh, you decide to head back to Alan to see if this effort has truly led you to victory...\n", Triggered: false}
 
@@ -669,5 +633,45 @@ func main() {
 				fmt.Println("Unknown command:", command)
 			}
 		}
+	}
+}
+
+func validInteraction() {
+	validInteractions = []*Interaction{
+		{
+			ItemName:   "tea",
+			EntityName: "rosie",
+			Event:      &Event{Description: "get-your-lanyard", Outcome: "Cheers! I needed that... by the way, where is your lanyard? I must have forgotten to give it to you.\nYou'll need that to move between rooms, here it is.\n\n(lanyard can now be found in the room).\n", Triggered: false},
+		},
+		{
+			ItemName:   "first-plate",
+			EntityName: "dishwasher",
+			Event:      &Event{Description: "first-plate-loaded", Outcome: "You loaded the first plate into the dishwasher.", Triggered: false},
+		},
+		{
+			ItemName:   "second-plate",
+			EntityName: "dishwasher",
+			Event:      &Event{Description: "second-plate-loaded", Outcome: "You loaded the second plate into the dishwasher.", Triggered: false},
+		},
+		{
+			ItemName:   "third-plate",
+			EntityName: "dishwasher",
+			Event:      &Event{Description: "third-plate-loaded", Outcome: "You loaded the third plate into the dishwasher.", Triggered: false},
+		},
+		{
+			ItemName:   "fourth-plate",
+			EntityName: "dishwasher",
+			Event:      &Event{Description: "fourth-plate-loaded", Outcome: "You loaded the fourth plate into the dishwasher.", Triggered: false},
+		},
+		{
+			ItemName:   "fifth-plate",
+			EntityName: "dishwasher",
+			Event:      &Event{Description: "fifth-plate-loaded", Outcome: "You loaded the fifth plate into the dishwasher.", Triggered: false},
+		},
+		{
+			ItemName:   "sixth-plate",
+			EntityName: "dishwasher",
+			Event:      &Event{Description: "sixth-plate-loaded", Outcome: "You loaded the sixth plate into the dishwasher.", Triggered: false},
+		},
 	}
 }
